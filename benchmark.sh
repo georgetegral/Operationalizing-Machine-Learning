7004bab3a67ea02b581f00bd06a75bfa8ab13244
georgetegral <georgetegral@gmail.com>
2021-04-30 19:34:35 +0000

    Added benchmarking and complete notebook

--- benchmark.sh
+++ benchmark.sh
@@ -70,4 +70,4 @@
 # Total:        114  160  28.0    172     199#
 
 
- ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer REPLACE_WITH_KEY' http://REPLACE_WITH_API_URL/score
+ ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer qDeLaajx8eJcZzgaD13c0w1KVJEIlV4j' http://c2ad71b6-9251-49f3-bb3f-34e9eb883fa7.southcentralus.azurecontainer.io/score
